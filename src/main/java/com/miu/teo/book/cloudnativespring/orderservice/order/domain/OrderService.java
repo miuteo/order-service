1eeba8c9ed8a4f3a4c51ad2d843600038a020015
miuteo <teodor.miu@outlook.com>
2025-02-24 21:40:48 +0200

    error handling for bookClient

--- src/main/java/com/miu/teo/book/cloudnativespring/orderservice/order/domain/OrderService.java
+++ src/main/java/com/miu/teo/book/cloudnativespring/orderservice/order/domain/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> buildAcceptedOrder(book, quantity))
-                .defaultIfEmpty( buildRejectedOrder(isbn,quantity)) //TODO 404 not found
+                .defaultIfEmpty( buildRejectedOrder(isbn,quantity))
                 .flatMap(orderRepository::save);
     }
 
